52ad7726bb1346501026adf21f9c95b0aebd3228
João M. D. Moura <joaomdmoura@gmail.com>
2017-12-03 21:12:29 -0200

    Adding initial state machine DSL
    This commit adds support for the initial DSL for the states and transitions
    it also adds the metaprogramming code that generates the functions for
    transitions and the proper validation of allowed transitions and states.

--- lib/machinery.ex
+++ lib/machinery.ex
@@ -1,18 +1,79 @@
 defmodule Machinery do
   @moduledoc """
-  Documentation for Machinery.
+  Main Machinery module.
+  It keeps the bunk of the Machinery logics, it's the module that
+  will be imported with `use` on the module that the state machine will
+  be implemented.
+
+   ## Parameters
+
+    - opts: A Keyword including `states` and `transitions`.
+
+  ## Example
+    ```
+    defmodule Project.User do
+      use Machinery,
+        states: [:created, :partial, :complete],
+        transitions: [
+          %{from: :created, to: [:partial, :complete]},
+          %{from: :partal, to: :complete}
+        ]
+    end
+    ```
   """
 
   @doc """
-  Hello world.
+  Main macro function that will be executed upon the load of the
+  module using it.
 
-  ## Examples
-
-      iex> Machinery.hello
-      :world
-
+  It basically stores the states and transition
   """
-  def hello do
-    :world
+  defmacro __using__(opts) do
+    states = Keyword.get(opts, :states)
+    transitions = Keyword.get(opts, :transitions)
+
+    # Quoted response to be inserted on the abstract syntax tree (AST) of
+    # the module that imported this using `use`.
+    quote bind_quoted: [states: states, transitions: transitions] do
+
+      # Mapping the declared states to create the functions for each one.
+      Enum.map(states, fn(state) ->
+
+        @doc """
+        Triggers the transition of a struct to a new state if it passes the
+        existing guard clause, also runs any before or after callbacks.
+        It returns a tuple with {:ok, state}, or {:error, "cause"}.
+
+        ## Parameters
+
+          - struct: A Struct based on a module using Machinery.
+          - next_state: Atom of the next state you want to transition to.
+
+        ## Examples
+
+            iex> User.transition_to(%User{state: partial}, :completed)
+            {:ok, %User{state: completed}}
+
+        """
+        def transition_to(struct, unquote(state) = next_state) do
+          current_state = Map.get(struct, :state)
+          if allowed_transition?(current_state, next_state) do
+            struct = Map.put(struct, :state, next_state)
+            {:ok, struct}
+          else
+            {:error, "Transition to this state isn't allowed"}
+          end
+        end
+      end)
+
+      defp allowed_transition?(current_state, next_state) do
+        transitions = unquote(Macro.escape(transitions))
+        case Map.fetch(transitions, current_state) do
+          {:ok, [_|_] = allowed_states} -> Enum.member?(allowed_states, next_state)
+          {:ok, allowed_state} -> allowed_state == next_state
+          :error -> false
+        end
+      end
+    end
   end
 end
--- test/machinery_test.exs
+++ test/machinery_test.exs
@@ -1,8 +1,29 @@
 defmodule MachineryTest do
   use ExUnit.Case
+  alias MachineryTest.TestModule
   doctest Machinery
 
-  test "greets the world" do
-    assert Machinery.hello() == :world
+  defmodule TestModule do
+    use Machinery,
+      states: [:created, :partial, :completed],
+      transitions: %{
+        created: [:partial, :completed],
+        partial: :completed
+      }
+  end
+
+  test "All transition_to methods for each state were injected into AST" do
+    assert :erlang.function_exported(TestModule, :transition_to, 2)
+    assert TestModule.transition_to(%{}, :created)
+    assert TestModule.transition_to(%{}, :partial)
+    assert TestModule.transition_to(%{}, :completed)
+  end
+
+  test "Only the declared transitions should be valid" do
+    assert {:ok, %{state: :partial}} = TestModule.transition_to(%{state: :created}, :partial)
+    assert {:ok, %{state: :completed}} = TestModule.transition_to(%{state: :created}, :completed)
+    assert {:ok, %{state: :completed}} = TestModule.transition_to(%{state: :partial}, :completed)
+    assert {:error, "Transition to this state isn't allowed"} = TestModule.transition_to(%{}, :created)
+    assert {:error, "Transition to this state isn't allowed"} = TestModule.transition_to(%{state: :completed}, :created)
   end
 end
