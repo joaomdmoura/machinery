5b267422243f1aab86c17e5ac81eaf046e3bf754
João M. D. Moura <joaomdmoura@gmail.com>
2017-12-04 13:47:26 -0200

    Updating README and setting up overall project info for release

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Machinery
 
-**TODO: Add description**
+[![Build Status](https://travis-ci.org/joaomdmoura/machinery.svg?branch=master)](https://travis-ci.org/joaomdmoura/machinery)
+[![Coverage Status](https://coveralls.io/repos/github/joaomdmoura/machinery/badge.svg?branch=master)](https://coveralls.io/github/joaomdmoura/machinery?branch=master)
+[![Ebert](https://ebertapp.io/github/joaomdmoura/machinery.svg)](https://ebertapp.io/github/joaomdmoura/machinery)
+
+![Machinery](https://github.com/joaomdmoura/machinery/blob/master/logo.png)
+
+Machinery is a State Machine library for structs in general.
+It also aims to have (when implemented with Phoenix) an optional build-in GUI
+that will represent each resource's state.
 
 ## Installation
 
--- mix.exs
+++ mix.exs
@@ -4,10 +4,18 @@ defmodule Machinery.Mixfile do
   def project do
     [
       app: :machinery,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/joaomdmoura/machinery",
+      docs: [
+        main: "Machinery",
+        logo: "https://github.com/joaomdmoura/machinery/blob/master/logo.png",
+        extras: ["README.md"]
+      ],
       test_coverage: [tool: ExCoveralls]
     ]
   end
@@ -22,7 +30,22 @@ defmodule Machinery.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.7", only: :test}
+      {:excoveralls, "~> 0.7", only: :test},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Machinery is a State Machine library for structs in general.
+    It support guard clauses, callbacks and integrate out of the box
+    with Phoenix apps."
+  end
+
+  defp package() do
+    [
+      maintainers: ["João M. D. Moura"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/joaomdmoura/machinery"}
     ]
   end
 end
