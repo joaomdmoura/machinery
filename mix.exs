799111c2ce61e06b3cefc614897d3a637d0a9e64
João M. D. Moura <joaomdmoura@gmail.com>
2017-12-04 12:12:55 -0200

    Adding Travis as CI and integrating project with coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Machinery.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -21,8 +22,7 @@ defmodule Machinery.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.7", only: :test}
     ]
   end
 end
