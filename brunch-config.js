b1690479356d4de6d0c93f0f084bc4a6820e507f
João M. D. Moura <joaomdmoura@gmail.com>
2017-12-26 13:36:30 -0200

    reorganizing css and fixing css issues

--- brunch-config.js
+++ brunch-config.js
@@ -1,37 +1,38 @@
+/*global exports */
 exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js'
 
       // To use a separate vendor.js bundle, specify two files path
       // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
       // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
+      //  'js/app.js': /^(web\/static\/js)/,
+      //  'js/vendor.js': /^(web\/static\/vendor)|(deps)/
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
       // https://github.com/brunch/brunch/tree/master/docs#concatenation
       // order: {
       //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
+      //     'web/static/vendor/js/jquery-2.1.1.js',
+      //     'web/static/vendor/js/bootstrap.min.js'
       //   ]
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: 'css/app.css'
     },
     templates: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js'
     }
   },
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
+    // By default, we set this to '/web/static/assets'. Files in this directory
+    // will be copied to `paths.public`, which is 'priv/static' by default.
     assets: /^(lib\/web\/static\/assets)/
   },
 
@@ -39,11 +40,11 @@ exports.config = {
   paths: {
     // Dependencies and current project directories to watch
     watched: [
-      "lib/web/static"
+      'lib/web/static'
     ],
 
     // Where to compile files to
-    public: "priv/static"
+    public: 'priv/static'
   },
 
   // Configure your plugins
@@ -56,7 +57,7 @@ exports.config = {
 
   modules: {
     autoRequire: {
-      "js/app.js": ["lib/web/static/js/app"]
+      'js/app.js': ['lib/web/static/js/app']
     }
   }
 };
