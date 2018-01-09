18e75d326018268f917373e6ae47749304fc65e9
João M. D. Moura <joaomdmoura@gmail.com>
2018-01-09 14:22:32 -0200

    Testing different layout

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -284,7 +284,7 @@ let renderResource = function(model, resource) {
   </li>
 
   <div class="modal fade" id="modal${resource.id}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">${model} - #${resource.id}</h5>
